31e5fa1d3ad32209a64e72b9b8148c48ab28a25d
randomguy2828 <gorgogaduma.gmail.com>
2025-11-14 22:55:15 +0400

    15th commit

---
+++ level 91/homework/hm1/script.js
@@ -0,0 +1,6 @@
+// შექმენით 5 ელემენტიანი სია, სიიდან ამოშალეთ ბოლო ელემენტი და ჩაამატეთ ახალი ელემენტი.
+
+list = ["apple", "lemon", "orange", "banana", "kiwi"]
+
+list.pop()
+list.push("grapefruit")
