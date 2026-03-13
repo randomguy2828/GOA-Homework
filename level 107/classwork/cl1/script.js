ff5ec42b6630a6b53637d4018161bba59e861151
randomguy2828 <gorgogaduma.gmail.com>
2026-03-13 17:46:09 +0400

    32th commit

---
+++ level 107/classwork/cl1/script.js
@@ -0,0 +1,21 @@
+// შემქნით ცვლადი სადაც შეინახავთ რიცხვს. ტერნატი ოპერატორის გამოყენებით დააბრუნეთ შესაბამისი თვე.
+
+let monthNum = 1
+
+
+let month = 
+monthNum === 1 ? console.log("იანვარი") :
+monthNum === 2 ? console.log("თებერვალი") :
+monthNum === 3 ?  console.log("მარტი") :
+monthNum === 4 ?  console.log("აპრილი" ):
+monthNum === 5 ?  console.log("მაისი" ):
+monthNum === 6 ?  console.log("ივნისი") :
+monthNum === 7 ?  console.log("ივლისი"):
+monthNum === 8 ?  console.log("აგვისტო") :
+monthNum === 9 ?  console.log("სექტემბერი") :
+monthNum === 10 ?  console.log("ოქტომბერი") :
+monthNum === 11 ?  console.log("ნოემბერი") :
+monthNum === 12 ?  console.log("დეკემბერი") :
+
+console.log(month)
+ 
