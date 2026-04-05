a624d452865174c477c9f92dc6e8dcb8f9664eae
randomguy2828 <gorgogaduma.gmail.com>
2026-04-05 11:20:47 +0400

    38th commit

--- level 110/homework/hm5/script.js
+++ level 110/homework/hm5/script.js
@@ -1,3 +1,8 @@
 // შექმენით ცვლადი სადაც შეინახავთ მინიმუმ 20 წიმბოლოიან სტრინგს სადაც იქნება სფეისებიც. გადააქციეთ ეს სტრინგი სიად და დააბრუნეთ პირველივე ხმოვანი რომელ ინდექსზეა.
 
-// პირობა ვერ მივხვდი რა არის წიმბოლოიან?
+let str = "mokled ar momewona es davaleba aravis ar vurchev amis gaketebas"
+
+let char = str.split(" ")
+let vowel = "aeiou"
+
+let first = char.findIndex((txt) => vowel.includes(txt))
--- level 110/homework/hm6/script.js
+++ level 110/homework/hm6/script.js
@@ -1,3 +1,15 @@
 // შექმენით სია სადაც იქნება რიცხვები. დააბრუნეთ პირველივე რიცხვი რომელიც არის მარტივი რიცხვი.
 
-// ???
+let numbers = [4, 68, 8, 9, 17]
+
+let num1 = numbers.find(num => {
+    if(num < 2) return false
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+    return true            
+})
+
+console.log(num1)
